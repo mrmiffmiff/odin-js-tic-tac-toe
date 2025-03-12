a215642a7c9d09566188edfa85004a67786d1d30
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-03-11 17:31:30 -0700

    Set up html file and script; add capability to create squares and manipulate them

---
+++ tictactoe.js
@@ -0,0 +1,19 @@
+/* To start, we need to think about how we'll represent individual squares on the board.
+ * The Gameboard object (coming later) could easily just hold a 2D array of strings that hold the state.
+ * However, I think it's more effective if each square is represented by an object in itself.
+ * This boardSquare object can hold a string, but can also have methods on it.
+ * This would make manipulation and state changes both easier and more secure. */
+function boardSquare() {
+    let sign = ""; // Obviously need to initialize as empty.
+
+    // Getters and setters
+    const getSign = () => sign;
+    const setSign = (playerSign) => {
+        sign = playerSign;
+    }
+
+    return { getSign, setSign };
+}
+
+// For this initial example, let's just have a single sample square and test that the methods work.
+const sampleSquare = boardSquare();
