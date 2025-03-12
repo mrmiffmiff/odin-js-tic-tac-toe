fed3c766c0ee26a0ab7e80ef213ef1bef549a048
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-03-11 21:19:44 -0700

    Create gameboard object with IIFE factory, move boardSquare factory inside

--- tictactoe.js
+++ tictactoe.js
@@ -1,20 +1,35 @@
-/* To start, we need to think about how we'll represent individual squares on the board.
- * The Gameboard object (coming later) could easily just hold a 2D array of strings that hold the state.
- * However, I think it's more effective if each square is represented by an object in itself.
- * This boardSquare object can hold a string, but can also have methods on it.
- * This would make manipulation and state changes both easier and more secure. */
-function boardSquare() {
-    let sign = ""; // Obviously need to initialize as empty.
+/* Let's create a Gameboard object using an IIFE (we only need the one).
+ * The board array is a 2D array containing all information about the tic-tac-toe board.
+ * I opted to make each square represented by its own object. I'll explain why above the relevant line. */
+const Gameboard = (function gameboard() {
+    let board = [];
+    const rows = 3;
+    const columns = 3;
 
-    // Getters and setters
-    const getSign = () => sign;
-    const setSign = (playerSign) => {
-        if (sign != "") throw new Error("Square already assigned!"); // We can catch this in the gameboard or gameflow (haven't decided as of this comment)
-        sign = playerSign;
+    // Now obviously the board is a square so the above lines may be somewhat unnecessary, but better to abstract a bit.
+    // Choosing to have the first dimension of the array be rows and the second dimension be columns was purely an ad-hoc decision.
+    // It does not really make a difference.
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            /* The gameboard could have easily held a 2D array of strings with the state.
+             * But it's more effective for reach square to be represented by an object in itself, so that it could have methods on it.
+             * Manipulation is thus much easier in this way, and more secure.
+             * And while it does seem somewhat silly to invokve an IIFE inside a loop, ultimately we don't need more than 9 squares. */
+            board[i][j] = (function boardSquare() {
+                let sign = ""; // Obviously need to initialize as empty.
+
+                // Getters and setters
+                const getSign = () => sign;
+                const setSign = (playerSign) => {
+                    if (sign != "") throw new Error("Square already assigned!"); // We can catch this in the gameboard or gameflow (haven't decided as of this comment)
+                    sign = playerSign;
+                }
+
+                return { getSign, setSign };
+            })();
+        }
     }
 
-    return { getSign, setSign };
-}
-
-// For this initial example, let's just have a single sample square and test that the methods work.
-const sampleSquare = boardSquare();
+    return { board }; // Probably a temporary thing
+})();
